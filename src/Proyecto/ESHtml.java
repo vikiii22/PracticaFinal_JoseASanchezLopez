6cf1429386ee5e9a9fc00f6c286c643e8092cba9
Jose <80800807+vikiii22@users.noreply.github.com>
2021-06-06 02:21:37 +0200

    añadida salida HTML

--- src/Proyecto/ESHtml.java
+++ src/Proyecto/ESHtml.java
@@ -1,7 +1,20 @@
 package Proyecto;
 
+import java.io.IOException;
+import java.io.UTFDataFormatException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class ESHtml {
     public static void escribeHTML(int total){
-        
+        String salida="<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"estilo.css\" media=\"screen\"/></head>";
+        salida+="<body><h1>Total a pagar: </h1><h2>"+total+"€</h2></body></html>";
+        try {
+            Files.writeString(Paths.get("pagina.html"), salida, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
